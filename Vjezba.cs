dbc05f2ccfc12e131c2aa96be3cc320ea3cfb883
teosrebrnic <teo.srebrnic@skole.hr>
2026-03-03 11:11:52 +0100

    Add files via upload

---
+++ Vjezba.cs
@@ -0,0 +1,38 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string poruka = "Odaberite Yes / No / Cancel";
+
+            string naslov = "Naslovna linija Dijaloga za poruke";
+
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+
+            MessageBoxIcon ikona = MessageBoxIcon.Information;
+            // MessageBox.Show() vraća vrijednost tipa DialogResult
+
+            // tu vrijednost dodjeljujemo varijabli rez
+
+            DialogResult rez = MessageBox.Show(poruka, naslov, buttons, ikona);
+            switch (rez)
+            {
+                case DialogResult.Yes:
+                    textBoxRezultat.Text = "Izabrali ste dugme YES";
+                    break;
+                case DialogResult.No:
+                    textBoxRezultat.Text = "Izabrali ste dugme NO";
+                    break;
+                case DialogResult.Cancel:
+                    textBoxRezultat.Text = "Izabrali ste dugme Cancel";
+                    break;
+            }
+        }
+    }
+}
